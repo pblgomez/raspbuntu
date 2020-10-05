00a4eeddeafc5c4e92c2116f7a87b0867437a779
Pablo Gómez <pablogomez@pablogomez.com>
2020-10-06 00:11:30 +0100

    Update 00-initial.sh
    Better compatibility for copying ssh keys

--- 00-initial.sh
+++ 00-initial.sh
@@ -3,4 +3,5 @@
 echo "Ingresa la ip de la raspberry"
 read ip
 ssh ubuntu@$ip          # First one to change the default password
-ssh-copy-id -i ~/.ssh/id_ed25519.pub ubuntu@$ip
+#ssh-copy-id -i ~/.ssh/id_ed25519.pub ubuntu@$ip
+cat ~/.ssh/id_rsa.pub | ssh ubuntu@$ip 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
